5c191390158b331c3a67e7cffe1dfe7f72d3ffb7
ICHFO <hans.fonteyn@infocura.be>
2018-11-20 10:32:24 +0100

    Added DevConfig class

--- config.py
+++ config.py
@@ -5,3 +5,10 @@ class Config(object):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     UPLOAD_FOLDER = 'uploads'
+    
+class DevConfig(Config):
+    ctd_server = 192.168.1.246
+    ctd_database = "ctddev"
+    ctd_port = 50010
+    ctd_user = "db2dev"
+    ctd_password = "SuadaSoft"
